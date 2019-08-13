82a8a13df7211308ff0c5470c89f054a0b7fd90c
Acrimon <joel.wejdenstal@gmail.com>
2019-08-13 10:45:50 +0200

    Removed uneeded move in front of closure in web::start.

--- server/src/web.rs
+++ server/src/web.rs
@@ -1,7 +1,7 @@
 use rouille::{router, Request, Response};
 
 pub fn start() {
-    rouille::start_server("0.0.0.0:19253", move |req| handler(req));
+    rouille::start_server("0.0.0.0:19253", |req| handler(req));
 }
 
 fn handler(req: &Request) -> Response {
