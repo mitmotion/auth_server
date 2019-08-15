c1cde9b404e2913201d1d88c485bb6b4457da08f
Acrimon <joel.wejdenstal@gmail.com>
2019-08-15 12:11:41 +0200

    Added bindings for uuid lookup.

--- authc/src/lib.rs
+++ authc/src/lib.rs
@@ -1,6 +1,7 @@
 pub use auth_common::AuthToken;
 use std::env;
 pub use uuid::Uuid;
+use auth_common::{SignInResponse, ValidityCheckResponse, UuidLookupResponse};
 
 fn resolve_provider() -> String {
     env::var("VELOREN_AUTH_PROVIDER").unwrap_or("https://auth.veloren.net".to_owned())
@@ -44,6 +45,23 @@ impl AuthClient {
             .send()?;
         if resp.status().is_success() {
             Ok(())
+        } else {
+            Err(AuthClientError::GenericError)
+        }
+    }
+
+    pub fn username_to_uuid(&self, username: impl AsRef<str>) -> Result<Uuid, AuthClientError> {
+        let username = username.as_ref();
+        let ep = format!("{}/api/v1/utuuid", self.provider);
+        let mut resp = self
+            .http
+            .get(&ep)
+            .query(&[("username", username)])
+            .send()?;
+        if resp.status().is_success() {
+            let body = resp.text()?;
+            let data: UuidLookupResponse = serde_json::from_str(body.as_str())?;
+            Ok(data.uuid)
         } else {
             Err(AuthClientError::GenericError)
         }
