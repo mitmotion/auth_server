a78e3aa077b7a890da079a5090f0daf99b301f2e
Acrimon <joel.wejdenstal@gmail.com>
2019-08-15 11:51:35 +0200

    Added a register api.

--- authc/src/lib.rs
+++ authc/src/lib.rs
@@ -1,7 +1,51 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub use auth_common::AuthToken;
+use std::env;
+pub use uuid::Uuid;
+
+fn resolve_provider() -> String {
+    env::var("VELOREN_AUTH_PROVIDER").unwrap_or("https://auth.veloren.net".to_owned())
+}
+
+pub enum AuthClientError {
+    GenericError,
+}
+
+impl<E: std::error::Error> From<E> for AuthClientError {
+    fn from(_error: E) -> Self {
+        AuthClientError::GenericError
+    }
+}
+
+pub struct AuthClient {
+    http: reqwest::Client,
+    provider: String,
+}
+
+impl AuthClient {
+    pub fn new() -> Self {
+        Self {
+            http: reqwest::Client::new(),
+            provider: resolve_provider(),
+        }
+    }
+
+    pub fn register(
+        &self,
+        username: impl AsRef<str>,
+        password: impl AsRef<str>,
+    ) -> Result<(), AuthClientError> {
+        let username = username.as_ref();
+        let password = password.as_ref();
+        let ep = format!("{}/api/v1/register", self.provider);
+        let resp = self
+            .http
+            .get(&ep)
+            .query(&[("username", username), ("password", password)])
+            .send()?;
+        if resp.status().is_success() {
+            Ok(())
+        } else {
+            Err(AuthClientError::GenericError)
+        }
     }
 }
