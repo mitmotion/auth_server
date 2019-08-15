5be3d87dbb6745e8ea96371a99a2cc430b3bc62d
Acrimon <joel.wejdenstal@gmail.com>
2019-08-15 12:24:24 +0200

    Added signin and validate api bindings.

--- authc/src/lib.rs
+++ authc/src/lib.rs
@@ -1,7 +1,8 @@
 pub use auth_common::AuthToken;
+use auth_common::{SignInResponse, UuidLookupResponse, ValidityCheckResponse};
 use std::env;
+use std::net::Ipv4Addr;
 pub use uuid::Uuid;
-use auth_common::{SignInResponse, ValidityCheckResponse, UuidLookupResponse};
 
 fn resolve_provider() -> String {
     env::var("VELOREN_AUTH_PROVIDER").unwrap_or("https://auth.veloren.net".to_owned())
@@ -53,14 +54,53 @@ impl AuthClient {
     pub fn username_to_uuid(&self, username: impl AsRef<str>) -> Result<Uuid, AuthClientError> {
         let username = username.as_ref();
         let ep = format!("{}/api/v1/utuuid", self.provider);
-        let mut resp = self
-            .http
-            .get(&ep)
-            .query(&[("username", username)])
-            .send()?;
+        let mut resp = self.http.get(&ep).query(&[("username", username)]).send()?;
         if resp.status().is_success() {
             let body = resp.text()?;
             let data: UuidLookupResponse = serde_json::from_str(body.as_str())?;
+            Ok(data.uuid)
+        } else {
+            Err(AuthClientError::GenericError)
+        }
+    }
+
+    pub fn sign_in(
+        &self,
+        username: impl AsRef<str>,
+        password: impl AsRef<str>,
+        server: Ipv4Addr,
+    ) -> Result<AuthToken, AuthClientError> {
+        let username = username.as_ref();
+        let password = password.as_ref();
+        let server = server.to_string();
+        let server = server.as_str();
+        let ep = format!("{}/api/v1/signin", self.provider);
+        let mut resp = self
+            .http
+            .get(&ep)
+            .query(&[
+                ("username", username),
+                ("password", password),
+                ("server", server),
+            ])
+            .send()?;
+        if resp.status().is_success() {
+            let body = resp.text()?;
+            let data: SignInResponse = serde_json::from_str(body.as_str())?;
+            Ok(data.token)
+        } else {
+            Err(AuthClientError::GenericError)
+        }
+    }
+
+    pub fn validate(&self, token: AuthToken) -> Result<Uuid, AuthClientError> {
+        let token = token.serialize();
+        let token = token.as_str();
+        let ep = format!("{}/api/v1/validate", self.provider);
+        let mut resp = self.http.get(&ep).query(&[("token", token)]).send()?;
+        if resp.status().is_success() {
+            let body = resp.text()?;
+            let data: ValidityCheckResponse = serde_json::from_str(body.as_str())?;
             Ok(data.uuid)
         } else {
             Err(AuthClientError::GenericError)
