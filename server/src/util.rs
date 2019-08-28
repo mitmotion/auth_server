7dce2a77d2647f960c80ecf849ad602f1bf56204
Acrimon <joel.wejdenstal@gmail.com>
2019-08-28 15:30:43 +0200

    Made the argon2 config a const.

--- server/src/util.rs
+++ server/src/util.rs
@@ -10,23 +10,23 @@ pub fn wrap_err<T, E: 'static + StdError + Sync + Send>(r: StdResult<T, E>) -> R
     r.map_err(|e| Error::from_boxed_compat(Box::new(e)))
 }
 
-pub fn hash(data: &[u8]) -> String {
-    let config = Config {
-        ad: &[],
-        hash_length: 64,
-        lanes: 1,
-        mem_cost: 4096,
-        secret: &[],
-        thread_mode: ThreadMode::Sequential,
-        time_cost: 1,
-        variant: Variant::Argon2d,
-        version: Version::Version13,
-    };
+const ARGON2CONFIG: Config = Config {
+    ad: &[],
+    hash_length: 64,
+    lanes: 1,
+    mem_cost: 4096,
+    secret: &[],
+    thread_mode: ThreadMode::Sequential,
+    time_cost: 1,
+    variant: Variant::Argon2d,
+    version: Version::Version13,
+};
 
+pub fn hash(data: &[u8]) -> String {
     let mut salt: [u8; 64] = [0; 64];
     thread_rng().fill(&mut salt[..]);
 
-    argon2::hash_encoded(data, &salt, &config).unwrap()
+    argon2::hash_encoded(data, &salt, &ARGON2CONFIG).unwrap()
 }
 
 pub fn verify(hash: &str, data: &[u8]) -> bool {
