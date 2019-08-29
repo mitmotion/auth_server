0a23ddf20033609aec993e0f03988a7ae57cad97
Acrimon <joel.wejdenstal@gmail.com>
2019-08-29 08:24:15 +0200

    Only compile regexes on startup.

--- server/src/auth.rs
+++ server/src/auth.rs
@@ -36,8 +36,7 @@ fn ensure_within_len(s: String, l: usize) -> Result<String> {
 }
 
 fn ensure_valid_text(s: String) -> Result<String> {
-    let text_re = Regex::new(r#"^[[:word:]]*$"#).unwrap();
-    if text_re.is_match(&s) {
+    if USERNAME_RE.is_match(&s) {
         Ok(s)
     } else {
         Err(StringValidateError::InvalidText.into())
@@ -45,8 +44,7 @@ fn ensure_valid_text(s: String) -> Result<String> {
 }
 
 fn ensure_valid_email(s: String) -> Result<String> {
-    let email_re = Regex::new(r#"^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$"#).unwrap();
-    if email_re.is_match(&s) {
+    if EMAIL_RE.is_match(&s) {
         Ok(s)
     } else {
         Err(StringValidateError::InvalidEmail.into())
@@ -68,6 +66,8 @@ lazy_static! {
             .expect("failed to create pool")
     };
     static ref CACHE: ExpiryCache<Vec<u8>, Vec<u8>> = ExpiryCache::new();
+    static ref EMAIL_RE: Regex = Regex::new(r#"^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$"#).unwrap();
+    static ref USERNAME_RE: Regex = Regex::new(r#"^[[:word:]]*$"#).unwrap();
 }
 
 pub fn prepare_db() -> Result<()> {
