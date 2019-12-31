d1408c76cf3549e3329bbdd4b152bcbcf20c666f
Acrimon <joel.wejdenstal@gmail.com>
2019-12-31 20:41:32 +0100

    Finished up web part.

--- server/src/web.rs
+++ server/src/web.rs
@@ -1,4 +1,4 @@
-use rouille::{start_server, Request, Response};
+use rouille::{start_server, Request, Response, router};
 use std::error::Error;
 use auth_common::{RegisterPayload, UuidLookupPayload, UuidLookupResponse, SignInPayload, SignInResponse, ValidityCheckPayload, ValidityCheckResponse, UsernameLookupPayload, UsernameLookupResponse};
 use crate::auth::{self, AuthError};
@@ -10,11 +10,11 @@ fn err_handle<E: Error>(f: impl FnOnce() -> Result<Response, E>) -> Response {
     }
 }
 
-fn ping(req: Request) -> Response {
+fn ping(req: &Request) -> Response {
     Response::text(format!("Ping! {}", req.remote_addr().ip()))
 }
 
-fn username_to_uuid(req: Request) -> Result<Response, AuthError> {
+fn username_to_uuid(req: &Request) -> Result<Response, AuthError> {
     let body = req.data().unwrap();
     let payload: UuidLookupPayload = serde_json::from_reader(body)?;
     let uuid = auth::username_to_uuid(&payload.username)?;
@@ -22,7 +22,7 @@ fn username_to_uuid(req: Request) -> Result<Response, AuthError> {
     Ok(Response::json(&response))
 }
 
-fn uuid_to_username(req: Request) -> Result<Response, AuthError> {
+fn uuid_to_username(req: &Request) -> Result<Response, AuthError> {
     let body = req.data().unwrap();
     let payload: UsernameLookupPayload = serde_json::from_reader(body)?;
     let username = auth::uuid_to_username(&payload.uuid)?;
@@ -30,14 +30,14 @@ fn uuid_to_username(req: Request) -> Result<Response, AuthError> {
     Ok(Response::json(&response))
 }
 
-fn register(req: Request) -> Result<Response, AuthError> {
+fn register(req: &Request) -> Result<Response, AuthError> {
     let body = req.data().unwrap();
     let payload: RegisterPayload = serde_json::from_reader(body)?;
     auth::register(&payload.username, &payload.password)?;
     Ok(Response::text("Ok"))
 }
 
-fn generate_token(req: Request) -> Result<Response, AuthError> {
+fn generate_token(req: &Request) -> Result<Response, AuthError> {
     let body = req.data().unwrap();
     let payload: SignInPayload = serde_json::from_reader(body)?;
     let token = auth::generate_token(&payload.username, &payload.password)?;
@@ -45,10 +45,42 @@ fn generate_token(req: Request) -> Result<Response, AuthError> {
     Ok(Response::json(&response))
 }
 
-fn verify(req: Request) -> Result<Response, AuthError> {
+fn verify(req: &Request) -> Result<Response, AuthError> {
     let body = req.data().unwrap();
     let payload: ValidityCheckPayload = serde_json::from_reader(body)?;
     let uuid = auth::verify(payload.token)?;
     let response = ValidityCheckResponse { uuid };
     Ok(Response::json(&response))
+}
+
+pub fn start() {
+    start_server("0.0.0.0:19583", move |request| {
+        router!(request,
+            (GET) (/ping) => {
+                ping(request)
+            },
+
+            (POST) (/username_to_uuid) => {
+                err_handle(|| username_to_uuid(request))
+            },
+
+            (POST) (/uuid_to_username) => {
+                err_handle(|| uuid_to_username(request))
+            },
+
+            (POST) (/register) => {
+                err_handle(|| register(request))
+            },
+
+            (POST) (/generate_token) => {
+                err_handle(|| generate_token(request))
+            },
+
+            (POST) (/verify) => {
+                err_handle(|| verify(request))
+            },
+
+            _ => Response::empty_404()
+        )
+    });
 }
