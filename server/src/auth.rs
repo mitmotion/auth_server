07a7bcbbe4635a4a275273736994b94c21f0ee6b
Imbris <imbrisf@gmail.com>
2020-02-22 10:19:57 -0500

    Made insentive to capitalization within username, use  to improve efficiency, add rough description to readme for how to setup an auth server

--- server/src/auth.rs
+++ server/src/auth.rs
@@ -6,6 +6,7 @@ use rusqlite::{params, Connection, Error as DbError, NO_PARAMS};
 use serde_json::Error as JsonError;
 use std::error::Error;
 use std::fmt;
+use std::ops::Deref;
 use uuid::Uuid;
 
 lazy_static! {
@@ -18,6 +19,19 @@ fn db() -> Result<Connection, AuthError> {
 
 fn salt() -> [u8; 16] {
     rand::random::<u128>().to_le_bytes()
+}
+
+fn decapitalize(string: &str) -> LowercaseString {
+    LowercaseString(string.chars().flat_map(char::to_lowercase).collect())
+}
+
+struct LowercaseString(String);
+impl Deref for LowercaseString {
+    type Target = str;
+
+    fn deref(&self) -> &str {
+        &self.0
+    }
 }
 
 #[derive(Debug)]
@@ -96,7 +110,8 @@ pub fn init_db() -> Result<(), AuthError> {
     db()?.execute(
         "
         CREATE TABLE IF NOT EXISTS users (
-            uuid TEXT PRIMARY KEY,
+            uuid TEXT NOT NULL PRIMARY KEY,
+            username_lowercase TEXT NOT NULL UNIQUE,
             username TEXT NOT NULL UNIQUE,
             pwhash TEXT NOT NULL
         )
@@ -106,17 +121,20 @@ pub fn init_db() -> Result<(), AuthError> {
     Ok(())
 }
 
-fn user_exists(username: &str) -> Result<bool, AuthError> {
+fn user_exists(username_lowercase: &LowercaseString) -> Result<bool, AuthError> {
     let db = db()?;
-    let mut stmt = db.prepare("SELECT uuid FROM users WHERE username == ?1")?;
-    Ok(stmt.exists(params![username])?)
+    let mut stmt = db.prepare("SELECT uuid FROM users WHERE username_lowercase == ?1")?;
+    Ok(stmt.exists(params![username_lowercase.deref()])?)
 }
 
 pub fn username_to_uuid(username: &str) -> Result<Uuid, AuthError> {
+    let username_lowercase = decapitalize(username);
     let db = db()?;
-    let mut stmt = db.prepare("SELECT uuid FROM users WHERE username == ?1")?;
+    let mut stmt = db.prepare_cached("SELECT uuid FROM users WHERE username_lowercase == ?1")?;
     let result = stmt
-        .query_map(params![username], |row| row.get::<_, String>(0))?
+        .query_map(params![username_lowercase.deref()], |row| {
+            row.get::<_, String>(0)
+        })?
         .filter_map(|s| s.ok())
         .filter_map(|s| Uuid::parse_str(&s).ok())
         .next()
@@ -127,7 +145,7 @@ pub fn username_to_uuid(username: &str) -> Result<Uuid, AuthError> {
 pub fn uuid_to_username(uuid: &Uuid) -> Result<String, AuthError> {
     let db = db()?;
     let uuid = uuid.to_simple().to_string();
-    let mut stmt = db.prepare("SELECT username FROM users WHERE uuid == ?1")?;
+    let mut stmt = db.prepare_cached("SELECT username FROM users WHERE uuid == ?1")?;
     let result = stmt
         .query_map(params![uuid], |row| row.get::<_, String>(0))?
         .filter_map(|s| s.ok())
@@ -137,25 +155,28 @@ pub fn uuid_to_username(uuid: &Uuid) -> Result<String, AuthError> {
 }
 
 pub fn register(username: &str, password: &str) -> Result<(), AuthError> {
-    if user_exists(username)? {
+    let username_lowercase = decapitalize(username);
+    if user_exists(&username_lowercase)? {
         return Err(AuthError::UserExists);
     }
     let uuid = Uuid::new_v4().to_simple().to_string();
     let hconfig = argon2::Config::default();
     let pwhash = argon2::hash_encoded(password.as_bytes(), &salt(), &hconfig)?;
     db()?.execute(
-        "INSERT INTO users (uuid, username, pwhash) VALUES(?1, ?2, ?3)",
-        params![uuid, username, pwhash],
+        "INSERT INTO users (uuid, username_lowercase, username, pwhash) VALUES(?1, ?2, ?3, ?4)",
+        params![uuid, username_lowercase.deref(), username, pwhash],
     )?;
     Ok(())
 }
 
 /// Checks if the password is correct and that the user exists.
-fn is_valid(username: &str, password: &str) -> Result<bool, AuthError> {
+fn is_valid(username_lowercase: &LowercaseString, password: &str) -> Result<bool, AuthError> {
     let db = db()?;
-    let mut stmt = db.prepare("SELECT pwhash FROM users WHERE username == ?1")?;
+    let mut stmt = db.prepare_cached("SELECT pwhash FROM users WHERE username_lowercase == ?1")?;
     let result = stmt
-        .query_map(params![username], |row| row.get::<_, String>(0))?
+        .query_map(params![&username_lowercase.deref()], |row| {
+            row.get::<_, String>(0)
+        })?
         .filter_map(|s| s.ok())
         .filter_map(|correct| argon2::verify_encoded(&correct, password.as_bytes()).ok())
         .next()
@@ -164,11 +185,12 @@ fn is_valid(username: &str, password: &str) -> Result<bool, AuthError> {
 }
 
 pub fn generate_token(username: &str, password: &str) -> Result<AuthToken, AuthError> {
-    if !is_valid(username, password)? {
+    let username_lowercase = decapitalize(username);
+    if !is_valid(&username_lowercase, password)? {
         return Err(AuthError::InvalidLogin);
     }
 
-    let uuid = username_to_uuid(username)?;
+    let uuid = username_to_uuid(&username_lowercase)?;
     let token = AuthToken::generate();
     TOKENS.insert(token, uuid);
     Ok(token)
